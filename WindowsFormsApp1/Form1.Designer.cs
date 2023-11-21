24bf184b42c7ec861cfcc8236af4a0c56e06dbd4
LeChung0990 <leducchung8183@gmail.com>
2023-11-21 10:08:10 +0700

    Update

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.BTNConnect = new System.Windows.Forms.Button();
+            this.BTNDisconnect = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox5 = new System.Windows.Forms.ComboBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.BTNSend = new System.Windows.Forms.Button();
+            this.BTNRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // BTNConnect
+            // 
+            this.BTNConnect.Location = new System.Drawing.Point(12, 165);
+            this.BTNConnect.Name = "BTNConnect";
+            this.BTNConnect.Size = new System.Drawing.Size(75, 23);
+            this.BTNConnect.TabIndex = 0;
+            this.BTNConnect.Text = "Connect";
+            this.BTNConnect.UseVisualStyleBackColor = true;
+            this.BTNConnect.Click += new System.EventHandler(this.BTNConnect_Click);
+            // 
+            // BTNDisconnect
+            // 
+            this.BTNDisconnect.Location = new System.Drawing.Point(113, 165);
+            this.BTNDisconnect.Name = "BTNDisconnect";
+            this.BTNDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.BTNDisconnect.TabIndex = 1;
+            this.BTNDisconnect.Text = "Disconnect";
+            this.BTNDisconnect.UseVisualStyleBackColor = true;
+            this.BTNDisconnect.Click += new System.EventHandler(this.BTNDisconnect_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(208, 2);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.PanButtons2 = System.Windows.Forms.MouseButtons.None;
+            this.zedGraphControl1.PanModifierKeys = System.Windows.Forms.Keys.None;
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(624, 358);
+            this.zedGraphControl1.TabIndex = 3;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            this.zedGraphControl1.ZoomButtons = System.Windows.Forms.MouseButtons.None;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(67, 7);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "PortName";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(67, 34);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 6;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(67, 87);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(121, 21);
+            this.comboBox3.TabIndex = 7;
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Items.AddRange(new object[] {
+            "One",
+            "Two"});
+            this.comboBox4.Location = new System.Drawing.Point(67, 114);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(121, 21);
+            this.comboBox4.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "BaudRate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Parity";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "StopBit";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "DataBit";
+            // 
+            // comboBox5
+            // 
+            this.comboBox5.FormattingEnabled = true;
+            this.comboBox5.Location = new System.Drawing.Point(67, 61);
+            this.comboBox5.Name = "comboBox5";
+            this.comboBox5.Size = new System.Drawing.Size(121, 21);
+            this.comboBox5.TabIndex = 12;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 149);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(176, 10);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 254);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(178, 95);
+            this.listBox1.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 209);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Transmitter";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 225);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(91, 20);
+            this.textBox1.TabIndex = 17;
+            // 
+            // BTNSend
+            // 
+            this.BTNSend.Location = new System.Drawing.Point(112, 222);
+            this.BTNSend.Name = "BTNSend";
+            this.BTNSend.Size = new System.Drawing.Size(75, 23);
+            this.BTNSend.TabIndex = 18;
+            this.BTNSend.Text = "Send";
+            this.BTNSend.UseVisualStyleBackColor = true;
+            this.BTNSend.Click += new System.EventHandler(this.BTNSend_Click);
+            // 
+            // BTNRun
+            // 
+            this.BTNRun.Location = new System.Drawing.Point(112, 193);
+            this.BTNRun.Name = "BTNRun";
+            this.BTNRun.Size = new System.Drawing.Size(75, 23);
+            this.BTNRun.TabIndex = 19;
+            this.BTNRun.Text = "Run";
+            this.BTNRun.UseVisualStyleBackColor = true;
+            this.BTNRun.Click += new System.EventHandler(this.BTNRun_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(834, 361);
+            this.Controls.Add(this.BTNRun);
+            this.Controls.Add(this.BTNSend);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.BTNDisconnect);
+            this.Controls.Add(this.BTNConnect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.HelpButton = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LogicAnalyzer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button BTNConnect;
+        private System.Windows.Forms.Button BTNDisconnect;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button BTNSend;
+        private System.Windows.Forms.Button BTNRun;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+using System.IO.Ports;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] baud = { "9600", "19200", "57600", "115200" };
+        string[] databit = { "5", "6", "7", "8" };
+        public int tong;
+        public int x, y, z, t, u;
+        public string data = "";
+        double[] thoigian = new double[256];
+        double[] dulieu = new double[256];
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "Bieu do";
+            myPane.YAxis.Title.Text = "Level";
+            myPane.XAxis.Title.Text = "Timer";
+
+            RollingPointPairList list1 = new RollingPointPairList(2000);
+            RollingPointPairList list2 = new RollingPointPairList(2000);
+
+            LineItem duongline1 = myPane.AddCurve("Level", list1, Color.Black, SymbolType.None);
+            LineItem duongline2 = myPane.AddCurve("Level", list2, Color.Red, SymbolType.None);
+
+            myPane.XAxis.Scale.Min = 0;
+            myPane.XAxis.Scale.Max = 256;
+            myPane.XAxis.Scale.MinorStep = 1;
+            myPane.XAxis.Scale.MajorStep = 1;
+
+            myPane.YAxis.Scale.Min = 0;
+            myPane.YAxis.Scale.Max = 10;
+            myPane.YAxis.Scale.MinorStep = 1;
+            myPane.YAxis.Scale.MajorStep = 1;
+
+            zedGraphControl1.AxisChange();
+
+            string[] myport = SerialPort.GetPortNames();
+            comboBox1.Items.AddRange(myport);
+            comboBox2.Items.AddRange(baud);
+            comboBox5.Items.AddRange(databit);
+            comboBox3.Items.AddRange(Enum.GetNames(typeof(Parity)));
+
+        }
+        public void draw(double line1, double line2)
+        {
+            LineItem duongline1 = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            LineItem duongline2 = zedGraphControl1.GraphPane.CurveList[1] as LineItem;
+            if (duongline1 == null)
+                return;
+            if (duongline2 == null)
+                return;
+            IPointListEdit list1 = duongline1.Points as IPointListEdit;
+            IPointListEdit list2 = duongline2.Points as IPointListEdit;
+            if (list1 == null)
+                return;
+            if (list2 == null)
+                return;
+
+            list1.Add(tong, line1);
+            list2.Add(tong, line2);
+            zedGraphControl1.AxisChange();
+            zedGraphControl1.Invalidate();
+            tong += 2;
+
+        }
+
+        private void BTNConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+                //serialPort1.BaudRate = int.Parse(comboBox2.Text);
+                //serialPort1.DataBits = int.Parse(comboBox5.Text);
+                serialPort1.BaudRate = 9600;
+                serialPort1.DataBits = 8;
+                //serialPort1.Parity = (Parity)Enum.Parse(typeof(Parity), comboBox3.Text);
+                //serialPort1.StopBits = (StopBits)Enum.Parse(typeof(StopBits ), comboBox4.Text);
+                serialPort1.Parity = Parity.None;
+                serialPort1.StopBits = StopBits.One;
+                serialPort1.Open();
+                progressBar1.Value = 100;
+                BTNConnect.Enabled = false;
+                BTNDisconnect.Enabled = true;
+
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void BTNDisconnect_Click(object sender, EventArgs e)
+        {
+            try
+            { 
+                serialPort1.Close();
+                progressBar1.Value = 0;
+                BTNConnect.Enabled = true;
+                BTNDisconnect.Enabled = false;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        public override string ToString()
+        {
+            return y.ToString() + z.ToString() + t.ToString() + u.ToString();
+        }
+        //    //Invoke(new MethodInvoker(() => listBox1.Items.Add(data)));
+        //    //Invoke(new MethodInvoker(() => draw(int.Parse(data), int.Parse(data))));
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //if (serialPort1.ReadExisting() == "S")
+            //{
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (serialPort1.ReadExisting() != "")
+            //    {
+            //        if (i / 2 == 0)
+            //        {
+            //for(int i=0; i < 10; i++)
+            //{
+
+            x = serialPort1.ReadByte();
+            if (x == 255) //tin hieu bat dau
+            {
+                for(int i = 0; i <256; i++)
+                {
+                    thoigian[i] = serialPort1.ReadByte();
+                    dulieu[i] = serialPort1.ReadByte();
+                }
+                
+            } 
+            //data = x.ToString() + y.ToString() + z.ToString();
+
+        }
+
+        double[] xs = {0,1,2,3,4,5,6,7,8,9,10,0,1,2,3,4,5,6};
+        double[] ys1 = {0,0,1,1,0,0,1,1,0,0,0,0,1,1,1,0,0,1,1 };
+        
+        private void BTNSend_Click(object sender, EventArgs e)
+        {
+            zedGraphControl1.GraphPane.CurveList.Clear();
+            var curve1 = zedGraphControl1.GraphPane.AddCurve("Series A",thoigian, dulieu, Color.Blue);
+            curve1.Line.IsAntiAlias = true;
+            curve1.Symbol.IsVisible = false;
+
+            zedGraphControl1.GraphPane.Title.Text = "Bieu do";
+            zedGraphControl1.GraphPane.YAxis.Title.Text = "Level";
+            zedGraphControl1.GraphPane.XAxis.Title.Text = "Timer";
+
+            zedGraphControl1.GraphPane.XAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.GraphPane.YAxis.ResetAutoScale(zedGraphControl1.GraphPane, CreateGraphics());
+            zedGraphControl1.Refresh();
+
+
+        }
+
+        private void BTNRun_Click(object sender, EventArgs e)
+        {
+            serialPort1.Write("a");
+
+            //data = serialPort1.ReadExisting();
+            //x = (serialPort1.ReadByte()) - 48;
+            //data = x.ToString();
+        }
+
+
+    }
+}
